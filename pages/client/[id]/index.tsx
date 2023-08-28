ff7a3314bd6cc210d7b6e65817c36e864aa851dc
이상윤 <zaar625@gmail.com>
2023-08-28 11:05:13 +0900

    65.동적 라우트로 내비게이팅하기

--- pages/client/[id]/index.tsx
+++ pages/client/[id]/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 function ClientProjectPage() {
     return (
         <div>
             <h1>The Project of a Given Client</h1>
+            <ul>
+                <li><Link href='/client/max'> Maximilian</Link></li>
+            </ul>
         </div>
     )
 }
--- pages/client/index.tsx
+++ pages/client/index.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link"
+
 function ClientPage() {
-    return <div><h1>Client page</h1></div>
+    const clients = [
+        {id:'max', name:'Maximilian'},
+        {id:'menual', name:'Menual'},
+    ]
+
+    return (
+    <div>
+            <h1>Client page</h1>
+            <ul>
+                {clients.map(client => <li key={client.id}>
+                    <Link href={`/client/${client.id}`}>{client.name}</Link>
+                </li>)}
+            </ul>
+        </div>)
 }
 
 export default ClientPage
