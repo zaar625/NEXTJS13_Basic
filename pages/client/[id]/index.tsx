cd8b5b09199232ca250ccb4ea62fd6d991039985
이상윤 <zaar625@gmail.com>
2023-08-28 10:33:53 +0900

    61.중첩된 동적 라우트 & 경로 구축하기

---
+++ pages/client/[id]/index.tsx
@@ -0,0 +1,9 @@
+function ClientProjectPage() {
+    return (
+        <div>
+            <h1>The Project of a Given Client</h1>
+        </div>
+    )
+}
+
+export default ClientProjectPage
---
+++ pages/client/index.tsx
@@ -0,0 +1,5 @@
+function ClientPage() {
+    return <div><h1>Client page</h1></div>
+}
+
+export default ClientPage
