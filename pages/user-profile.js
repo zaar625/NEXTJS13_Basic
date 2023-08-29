c40d80c64fb900892086c8b6d0f2eceae6462443
이상윤 <zaar625@gmail.com>
2023-08-29 15:36:59 +0900

    서버사이드 렌더링에 getServerSideProps 사용하기

---
+++ pages/user-profile.js
@@ -0,0 +1,17 @@
+function UserProfilePage(props) {
+    console.log(props)
+    return (
+        <h1>{props.username}</h1>
+    )
+}
+
+export async function getServerSideProps(context) {
+    console.log('실행')
+    return {
+        props:{
+            username:'Max'
+        }
+    }
+}
+
+export default UserProfilePage
